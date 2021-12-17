9e2389f8375eec8272fe0f4165c2254a4e7580d0
Michal Szota <m.szota@vasco-electronics.com>
2021-12-17 15:30:59 +0100

    day 4 part 2

--- 2021/4/4.rb
+++ 2021/4/4.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'set'
+
 class Bingo
   attr_reader :numbers, :boards
 
@@ -8,34 +10,39 @@ class Bingo
     @boards = input[1..-1].map do |board|
       Board.new(board.split("\n"))
     end
+    @winners = []
   end
 
   def play
     numbers.each_with_index do |number, index|
-      boards.each{ |board| board.mark_number(number) }
+      boards.each do |board|
+        board.mark_number(number)
+      end
       next if index < 4
 
-      if (@winner = winners.first)
-        @winning_number = number
-        break
+      unless winners.empty?
+        @winners += winners
+        winners.each { |w| boards.delete(w) }
       end
     end
   end
 
   def result
-    @winner.unmarked_numbers.sum * @winning_number
+    pp @winners.first.unmarked_numbers.sum * @winners.first.lucky_number
+    pp @winners.last.unmarked_numbers.sum * @winners.last.lucky_number
   end
 
   def winners
-    boards.map { |board| board if board.check }.compact
+    boards.map { |board| board if board.wins? }.compact
   end
 end
 
 class Board
-  attr_reader :rows
+  attr_reader :rows, :lucky_number
 
   def initialize(rows)
-    @rows = rows.map { |row| row.split(' ').map { |n| Number.new(n.to_i)} }
+    @rows = rows.map { |row| row.split(' ').map { |n| Number.new(n.to_i) } }
+    @lucky_number = nil
   end
 
   def mark_number(number)
@@ -44,14 +51,22 @@ class Board
         n.mark! if n.value == number
       end
     end
+
+    @lucky_number = number if marked_line?
   end
 
-  def check
-    rows.any? { |row| row.all?(&:marked) } || rows.transpose.any? { |row| row.all?(&:marked) }
+  def wins?
+    !@lucky_number.nil?
   end
 
   def unmarked_numbers
-    rows.map { |row| row.map { |n| n.value unless n.marked }}.flatten.compact
+    rows.map { |row| row.map { |n| n.value unless n.marked } }.flatten.compact
+  end
+
+  private
+
+  def marked_line?
+    rows.any? { |row| row.all?(&:marked) } || rows.transpose.any? { |row| row.all?(&:marked) }
   end
 end
 
@@ -71,4 +86,4 @@ end
 content = File.read('./input.txt').split("\n\n")
 bingo = Bingo.new(content)
 bingo.play
-pp bingo.result
+bingo.result
