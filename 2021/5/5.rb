e2ef353bad821b3394bd2f75510be7246035e897
Michal Szota <m.szota@vasco-electronics.com>
2021-12-17 23:57:59 +0100

    Fix typos

--- 2021/5/5.rb
+++ 2021/5/5.rb
@@ -7,7 +7,7 @@ class Line
     @points = draw_points
   end
 
-  def stright?
+  def straight?
     x1 == x2 || y1 == y2
   end
 
@@ -16,20 +16,14 @@ class Line
   def draw_points
     x_p = x1 > x2 ? x1.downto(x2) : x1.upto(x2)
     y_p = y1 > y2 ? y1.downto(y2) : y1.upto(y2)
-
-    if stright?
-      [*x_p].product([*y_p])
-    else
-      [*x_p].zip([*y_p])
-    end
+    straight? ? [*x_p].product([*y_p]) : [*x_p].zip([*y_p])
   end
 end
 
-puts 'Initializing...'
 content = File.read('./input.txt').split("\n")
 lines_input = content.map { |line| line.split(' -> ') }.map { |line| line.map { |l| l.split(',').map(&:to_i) } }
-puts 'Drawing lines..'
 all_lines = lines_input.map { |input| Line.new(*input) }
-stright_lines = all_lines.select(&:stright?)
-puts stright_lines.flat_map(&:points).tally.values.count { |x| x > 1 }
+straight_lines = all_lines.select(&:straight?)
+
+puts straight_lines.flat_map(&:points).tally.values.count { |x| x > 1 }
 puts all_lines.flat_map(&:points).tally.values.count { |x| x > 1 }
